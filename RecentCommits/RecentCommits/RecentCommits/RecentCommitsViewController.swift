87d938e42081e345a12d1ac9325c9a5555d736ac
NaveenMagatala <naveenmagatala@gmail.com>
2020-10-07 17:35:54 -0400

    chore: construct tableview in view controller

--- RecentCommits/RecentCommits/RecentCommits/RecentCommitsViewController.swift
+++ RecentCommits/RecentCommits/RecentCommits/RecentCommitsViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class RecentCommitsViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    private let viewModel = RecentCommitsViewModel()
+    private var displayModel = [CommitsDisplayModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func bindView() {
+        
+    }
+    
+    private func bindViewModel() {
+        
+    }
 
 
+}
+
+extension RecentCommitsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecentCommitsTableViewCell") else {
+            return UITableViewCell()
+        }
+        return cell
+    }
 }
 
